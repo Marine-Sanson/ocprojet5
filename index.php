7f0ddada257372ba93d57846807399f7de797e8f
Marine-Sanson <marine_sanson@yahoo.fr>
2023-08-01 14:37:37 +0200

    [tech] #1 WIP autoload

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-require 'vendor/autoload.php';
 
+require 'vendor/autoload.php';
+use App\Demo\Demo;
+
+$demo = new Demo();
+print_r($demo->getDemo());
+
+print_r("toto");
---
+++ src/Demo.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App\Demo;
+class Demo {
+      public function __construct(){
+
+      }
+      public function getDemo(){
+            return "hello world";
+      }
+}
