9969f553236ccc2fa51a216d0bb40c58a53323c9
Marine-Sanson <106590863+Marine-Sanson@users.noreply.github.com>
2023-08-02 10:46:27 +0200

    Tech/1234 install composer and more (#73)
    * with composer config
    
    * with composer config #1
    
    * with bootstrap install #2
    
    * with twig added #4
    
    * gitignore modification to add vendor/autoload #7
    
    * [tech] #1 install composer globaly
    
    * [tech] #1 WIP autoload
    
    * [tech] #1 #3 with test files for composer and Twig
    
    * [tech] #2 with test file for Bootstrap
    
    * Delete vendor directory
    
    * [tech] delete class Test

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'vendor/autoload.php';
+use App\Demo\Demo;
+
+$test = new Demo();
+echo $test->getDemo();
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+      'cache' => false,
+]);
+
+echo $twig->render('index.html');
---
+++ src/Demo/Demo.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+namespace App\Demo;
+class Demo {
+      public function __construct(){
+
+      }
+      public function getDemo(){
+            return "hello world ! ";
+      }
+}
