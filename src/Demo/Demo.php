a5ee0fa8b827d704e9d162d4f53031f52d2453ad
Marine-Sanson <marine_sanson@yahoo.fr>
2023-08-02 15:33:43 +0200

    [tech] adding a few comments in Demo.php #75

--- src/Demo/Demo.php
+++ src/Demo/Demo.php
@@ -1,6 +1,17 @@
 <?php 
+/**
+ * This file is part of ocproject5
+ * 
+ * It's only a temporary file for test
+ */
 
 namespace App\Demo;
+
+/**
+ * Demo class to test
+ * 
+ * @author marine <marine_sanson@yahoo.fr>
+ */
 class Demo
 {
     public function __construct()
