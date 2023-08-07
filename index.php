d0342905b59a7396547cb6448ed0c5c690b9ed48
Marine-Sanson <marine_sanson@yahoo.fr>
2023-08-07 13:28:50 +0200

    [router] with router and a few files to test it #6

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 
     require 'vendor/autoload.php';
-    use App\Demo\Demo;
+    use App\services\Router;
 
-    $test = new Demo();
-    echo $test->getDemo();
-
-    $loader = new \Twig\Loader\FilesystemLoader('templates');
-    $twig = new \Twig\Environment(
-        $loader, [
-        'cache' => false,
-        ]
-    );
-
-    echo $twig->render('index.html');
+    $router = new Router();
+    $router->route();
---
+++ src/services/Router.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\services;
+
+class Router {
+
+      public function parseRoute()
+      {
+            $route = [];
+            if (isset($_GET["route"]))
+            {
+                  $routeParam = explode("/", $_GET["route"]);
+                  $route["route"] = $routeParam[0];
+            } else 
+            {
+                  $route["route"] = "home";
+            }
+
+            if (isset($routeParam) && count($routeParam) === 2)
+            {
+                  $id = $routeParam["1"];
+            } else {
+                  $id = null;
+            }
+
+            $route["param"] = $id;
+            return $route;
+      }
+
+      public function route()
+      {
+            $route = $this->parseRoute();
+            
+            require "templates/layout.phtml";
+      }
+}
