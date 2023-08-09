c60244fccdb035a47a6b62c5bc582f74c8dbe3f6
Marine-Sanson <marine_sanson@yahoo.fr>
2023-08-09 12:01:24 +0200

    add a blank line at the end of all files (after vscode config)

--- src/services/Router.php
+++ src/services/Router.php
@@ -3,33 +3,33 @@ namespace App\services;
 
 class Router {
 
-      public function parseRoute()
-      {
-            $route = [];
-            if (isset($_GET["route"]))
-            {
-                  $routeParam = explode("/", $_GET["route"]);
-                  $route["route"] = $routeParam[0];
-            } else 
-            {
-                  $route["route"] = "home";
-            }
+    public function parseRoute()
+    {
+        $route = [];
+        if (isset($_GET["route"]))
+        {
+                $routeParam = explode("/", $_GET["route"]);
+                $route["route"] = $routeParam[0];
+        } else 
+        {
+                $route["route"] = "home";
+        }
 
-            if (isset($routeParam) && count($routeParam) === 2)
-            {
-                  $id = $routeParam["1"];
-            } else {
-                  $id = null;
-            }
+        if (isset($routeParam) && count($routeParam) === 2)
+        {
+                $id = $routeParam["1"];
+        } else {
+                $id = null;
+        }
 
-            $route["param"] = $id;
-            return $route;
-      }
+        $route["param"] = $id;
+        return $route;
+    }
 
-      public function route()
-      {
-            $route = $this->parseRoute();
-            
-            require "src/views/layout.phtml";
-      }
+    public function route()
+    {
+        $route = $this->parseRoute();
+        
+        require "src/views/layout.phtml";
+    }
 }
