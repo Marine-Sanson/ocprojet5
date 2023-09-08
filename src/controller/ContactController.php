30d3a3a7452d10ec28caa81e1bebe92a872700ac
Marine-Sanson <106590863+Marine-Sanson@users.noreply.github.com>
2023-09-08 11:47:59 +0200

    Feature/#22 create contact controller and service (#107)
    * contact controller
    
    * contact service

---
+++ src/controller/ContactController.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * ContactController File Doc Comment
+ * 
+ * PHP Version 8.1.10
+ * 
+ * @category Controller
+ * @package  App\controller
+ * @author   Marine Sanson <marine_sanson@yahoo.fr>
+ * @license  https://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://www.blog.marinesanson.fr/ Not inline for the moment
+ */
+declare(strict_types=1);
+
+namespace App\controller;
+
+use App\service\TemplateInterface;
+
+/**
+ * ContactController Class Doc Comment
+ * 
+ * @category Controller
+ * @package  App\controller
+ * @author   Marine Sanson <marine_sanson@yahoo.fr>
+ * @license  https://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://www.blog.marinesanson.fr/ Not inline for the moment
+ */
+class ContactController
+{
+        /**
+     * Summary of template
+     * 
+     * @var TemplateInterface
+     */
+    public TemplateInterface $template;
+
+    /**
+     * Summary of _instance
+     * 
+     * @var ContactController
+     */
+    private static $_instance;
+
+    const URL = "contact";
+
+    /**
+     * Summary of __construct call an instance of TemplateInterface
+     * 
+     * @param TemplateInterface $template template engine
+     */
+    public function __construct(TemplateInterface $template)
+    {
+        $this->template = $template;
+    }
+
+     /**
+      * Summary of getInstance
+      * That method create the unique instance of the class, if it doesn't exist and return it
+      * 
+      * @param \App\service\TemplateInterface $template template engine
+      * 
+      * @return \App\controller\ContactController
+      */
+    public static function getInstance(TemplateInterface $template) :ContactController
+    { 
+        if (is_null(self::$_instance)) {
+            self::$_instance = new ContactController($template);  
+        }
+    
+        return self::$_instance;
+    }
+
+    
+}
---
+++ src/service/ContactService.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * ContactService File Doc Comment
+ * 
+ * PHP Version 8.1.10
+ * 
+ * @category Service
+ * @package  App\service
+ * @author   Marine Sanson <marine_sanson@yahoo.fr>
+ * @license  https://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://www.blog.marinesanson.fr/ Not inline for the moment
+ */
+declare(strict_types=1);
+
+namespace App\service;
+
+
+
+/**
+ * ContactService Class Doc Comment
+ * 
+ * @category Service
+ * @package  App\service
+ * @author   Marine Sanson <marine_sanson@yahoo.fr>
+ * @license  https://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://www.blog.marinesanson.fr/ Not inline for the moment
+ */
+class ContactService
+{
+    /**
+     * Summary of template
+     * 
+     * @var TemplateInterface
+     */
+    public TemplateInterface $template;
+
+    /**
+     * Summary of _instance
+     * 
+     * @var ContactService
+     */
+    private static $_instance;
+
+     /**
+      * Summary of getInstance
+      * That method create the unique instance of the class, if it doesn't exist and return it
+      * 
+      * @return \App\service\ContactService
+      */
+    public static function getInstance() :ContactService
+    { 
+        if (is_null(self::$_instance)) {
+            self::$_instance = new ContactService();  
+        }
+    
+        return self::$_instance;
+    }
+
+}
