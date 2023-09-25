ff728739a9ccf6d0cfadf97f6020c6860aac6210
Marine-Sanson <marine_sanson@yahoo.fr>
2023-09-25 15:32:47 +0200

    adding CommentRepository #42

---
+++ src/repository/CommentRepository.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * CommentRepository File Doc Comment
+ * 
+ * PHP Version 8.1.10
+ * 
+ * @category Repository
+ * @package  App\repository
+ * @author   Marine Sanson <marine_sanson@yahoo.fr>
+ * @license  https://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://www.blog.marinesanson.fr/ Not inline for the moment
+ */
+declare(strict_types=1);
+
+namespace App\repository;
+
+use App\service\DatabaseService;
+
+/**
+ * CommentRepository Class Doc Comment
+ * 
+ * @category Repository
+ * @package  App\repository
+ * @author   Marine Sanson <marine_sanson@yahoo.fr>
+ * @license  https://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://www.blog.marinesanson.fr/ Not inline for the moment
+ */
+class CommentRepository
+{
+    /**
+     * Summary of _db
+     * 
+     * @var DatabaseService $_db connection between PHP and a database server
+     */
+    private DatabaseService $_db;
+
+    
+    
+}
